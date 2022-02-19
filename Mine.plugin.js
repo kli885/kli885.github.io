0f3e7e95250bd78c156e67a50e51ba771cda0c42
kli885 <kli7@ualberta.ca>
2022-02-19 01:49:23 -0700

    Initial Commit

--- Mine.plugin.js
+++ Mine.plugin.js
@@ -6,27 +6,24 @@
  *  
  */
 
-module.exports = class mineCSS {
+module.exports = class Mine {
     load() {
-        if (!window.BDFDB_Global || !Array.isArray(window.BDFDB_Global.pluginQueue)) window.BDFDB_Global = Object.assign({}, window.BDFDB_Global, { pluginQueue: [] });
-        if (!window.BDFDB_Global.downloadModal) {
-            window.BDFDB_Global.downloadModal = true;
-            BdApi.showConfirmationModal("Library Missing", `The Library Plugin needed for ${config.info.name} is missing. Please click "Download Now" to install it.`, {
-                confirmText: "Download Now",
-                cancelText: "Cancel",
-                onCancel: _ => { delete window.BDFDB_Global.downloadModal; },
-                onConfirm: _ => {
-                    delete window.BDFDB_Global.downloadModal;
-                    this.downloadLibrary();
-                }
-            });
-        }
-        if (!window.BDFDB_Global.pluginQueue.includes(config.info.name)) window.BDFDB_Global.pluginQueue.push(config.info.name);
+
     };
     start() {
-        var hasMember = document.getElementsByClassName('membersWrap-3NUR2t');
-        if (hasMember.length > 0) {
-            console.log("EXISTS")
+        var elements = document.getElementsByClassName("layout-1LjVue")
+        var elementClicked = function (event) {
+            // console.log(event.currentTarget.parentNode)
+            try {
+                if (event.currentTarget.parentNode.getAttribute("aria-label").includes("direct")) {
+                    console.log(document.querySelector(".guilds-2JjMmN + :nth-child(2)"))
+                }
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].addEventListener('click', elementClicked, false);
         }
     };
     stop() { }
